91ba1bb5339a1285d3ab61201d08d7b706047384
monerooo <102408599+monerooo@users.noreply.github.com>
2022-04-02 11:21:09 +0200

    Code cleaned up v1

--- js/main.js
+++ js/main.js
@@ -19,10 +19,10 @@ function switchTheme(e) {
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
         localStorage.setItem('theme', 'dark');
+    } else {
+		document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
     }
-    else {        document.documentElement.setAttribute('data-theme', 'light');
-          localStorage.setItem('theme', 'light');
-    }    
 }
 
 toggleSwitch.addEventListener('change', switchTheme, false);
