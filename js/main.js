9d3c3f2b99290fedb0da8bed8c82b195f239c533
recanman <noreply@example.invalid>
2024-01-10 17:07:48 -0800

    add GNU LibreJS license information

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+// @license magnet:?xt=urn:btih:d3d9a9a6595521f9666a5e94cc830dab83b65699&dn=expat.txt Expat
+// The above is a GNU LibreJS specific license declaration. The Expat license is the same as the MIT license.
+// See: https://www.gnu.org/software/librejs/free-your-javascript.html#magnet-link-license
+
 // Saves themes in-between pages to LocalStorage.
 const themeSwitcher = document.getElementById('theme-switcher');
 
